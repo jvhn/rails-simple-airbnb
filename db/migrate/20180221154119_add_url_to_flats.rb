76408b0535119cb84f016e9a8c46124f62dfb173
Jason Vanhalwyn <jvanhalwyn@icloud.com>
2018-02-21 17:27:16 +0100

    creation of a simple airbnb (begining)

---
+++ db/migrate/20180221154119_add_url_to_flats.rb
@@ -0,0 +1,5 @@
+class AddUrlToFlats < ActiveRecord::Migration[5.1]
+  def change
+    add_column :flats, :picture_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Amsterdam',
+  address: '10 Thomas straat Amsterdam',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 88,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Lille',
+  address: '10 grand place Lille',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 120,
+  number_of_guests: 2
+)
